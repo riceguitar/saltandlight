2790beec006ac29baea8f11a056edebbf50857ab
David Sudarma <david@95west.co>
2016-03-05 13:39:43 -0800

    Initial Base Theme Commit

---
+++ wp-content/themes/saltandlight/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package saltandlight
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'saltandlight' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'saltandlight' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'saltandlight' ), 'saltandlight', '<a href="http://www.95west.co" rel="designer">95 West</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
